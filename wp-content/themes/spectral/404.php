772660a6c37470e94705fa98c9d10fde21e7a183
Gokhan Yurur <yururg@gmail.com>
2021-02-09 19:11:52 +0200

    404 page

--- wp-content/themes/spectral/404.php
+++ wp-content/themes/spectral/404.php
@@ -10,51 +10,16 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'spectral' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'spectral' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'spectral' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$spectral_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'spectral' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$spectral_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+    <article id="main">
+        <header>
+            <h2><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'spectral' ); ?></h2>
+        </header>
+        <section class="wrapper style5">
+            <div class="inner">
+                <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'spectral' ); ?></p>
+            </div>
+        </section>
+    </article>
 
 <?php
 get_footer();
